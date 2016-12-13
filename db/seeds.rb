1fc7058e90526cb7bfcb9fc7957bffdb19c2c239
MarkLombardiNelson <marklombardinelson@gmail.com>
2016-12-12 19:35:19 -0500

    Finished Explorer Mode Mostly From Memory

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+stories = Story.create(
+{ title: "I Am The Greatest", link: "http://www.google.com", email: "example1@eample.com"}
+{ title: "I Am The Kindest", link: "http://www.google.com", email: "example2@eample.com"}
+{ title: "I Am The Tallest", link: "http://www.google.com", email: "example3@eample.com"}
+)
